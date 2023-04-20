cc658f7ccc4cfbe6a6df4496bbc554f27a73ddb9
niilliiz <n.vatanara@mydoki.com>
2023-04-20 18:57:47 +0330

    first commit

---
+++ pages/posts/first-post.js
@@ -0,0 +1,24 @@
+import Link from "next/link";
+import Head from "next/head";
+import Script from "next/script";
+
+import Layout from "../../components/layout";
+
+export default function FirstPost() {
+  return (
+    <Layout>
+      <Head>
+        <title>first page</title>
+      </Head>
+      <Script
+        src="https://connect.facebook.net/en_US/sdk.js"
+        strategy="lazyOnload"
+        onLoad={() => console.log("script loaded correctly")}
+      />
+      <h1>First Post</h1>
+      <h2>
+        <Link href="/">Back to home</Link>
+      </h2>
+    </Layout>
+  );
+}
